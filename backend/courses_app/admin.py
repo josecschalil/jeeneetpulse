976de6ba492000d4c0208b04fb95d8d06d07868f
jose <josecschalil@gmail.com>
2025-01-26 19:13:32 +0530

    Edited Admin panel

--- backend/courses_app/admin.py
+++ backend/courses_app/admin.py
@@ -1,11 +1,82 @@
 from django.contrib import admin
-
-# admin.py
-
-from django.contrib import admin
-from .models import Course
+from .models import (
+    Course, Subject, Chapter, LectureVideo, LectureNote,
+    Exam, Question, UserCourseData, UserExamData,
+    ChapterQuestion, ExamQuestion
+)
 
 @admin.register(Course)
 class CourseAdmin(admin.ModelAdmin):
-    list_display = ('title', 'course_type', 'price', 'img')  # Show img in list view
-    search_fields = ('title', 'course_type')
+    list_display = ('title', 'course_type', 'price', 'current_price', 'validity')
+    search_fields = ('title', 'course_type', 'description')
+    list_filter = ('course_type',)
+    ordering = ('title',)
+
+
+@admin.register(Subject)
+class SubjectAdmin(admin.ModelAdmin):
+    list_display = ('name', 'type', 'course')
+    search_fields = ('name', 'course__title')
+    list_filter = ('type', 'course__title')
+    ordering = ('name',)
+
+
+@admin.register(Chapter)
+class ChapterAdmin(admin.ModelAdmin):
+    list_display = ('name', 'subject')
+    search_fields = ('name', 'subject__name', 'subject__course__title')
+    ordering = ('name',)
+
+
+@admin.register(LectureVideo)
+class LectureVideoAdmin(admin.ModelAdmin):
+    list_display = ('video_title', 'chapter')
+    search_fields = ('video_title', 'chapter__name', 'chapter__subject__name')
+
+
+@admin.register(LectureNote)
+class LectureNoteAdmin(admin.ModelAdmin):
+    list_display = ('chapter',)
+    search_fields = ('chapter__name', 'chapter__subject__name')
+
+
+@admin.register(Exam)
+class ExamAdmin(admin.ModelAdmin):
+    list_display = ('exam_title', 'time', 'diffculty', 'is_fullCourseExam', 'is_fullSubjectExam', 'is_fullChapterExam')
+    search_fields = ('exam_title',)
+    list_filter = ('diffculty', 'is_fullCourseExam', 'is_fullSubjectExam', 'is_fullChapterExam')
+    ordering = ('exam_title',)
+
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'question_text', 'level', 'correct_answer')
+    search_fields = ('question_text', 'concept_involved')
+    list_filter = ('level',)
+    ordering = ('id',)
+
+
+@admin.register(UserCourseData)
+class UserCourseDataAdmin(admin.ModelAdmin):
+    list_display = ('user', 'course', 'progress')
+    search_fields = ('user__username', 'course__title')
+    list_filter = ('progress',)
+
+
+@admin.register(UserExamData)
+class UserExamDataAdmin(admin.ModelAdmin):
+    list_display = ('user', 'exam_id', 'attempt_number', 'is_submitted')
+    search_fields = ('user__username', 'exam_id')
+    list_filter = ('is_submitted', 'is_active')
+
+
+@admin.register(ChapterQuestion)
+class ChapterQuestionAdmin(admin.ModelAdmin):
+    list_display = ('chapter', 'question')
+    search_fields = ('chapter__name', 'question__question_text')
+
+
+@admin.register(ExamQuestion)
+class ExamQuestionAdmin(admin.ModelAdmin):
+    list_display = ('exam', 'question')
+    search_fields = ('exam__exam_title', 'question__question_text')
--- backend/main_app/admin.py
+++ backend/main_app/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser, Profile
 
-# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ('email', 'name', 'is_staff', 'is_active')
+    list_filter = ('is_staff', 'is_active')
+    search_fields = ('email', 'name')
+    ordering = ('email',)
+
+    # Fields to display when adding or changing a user
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal Info', {'fields': ('name',)}),
+        ('Permissions', {'fields': ('is_staff', 'is_active', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login',)}),
+    )
+
+    # Fields to display when creating a new user
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'name', 'password1', 'password2', 'is_staff', 'is_active')}
+         ),
+    )
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'bio')
+    search_fields = ('user__email', 'bio')
+
+
+# Register the custom user admin
+admin.site.register(CustomUser, CustomUserAdmin)
